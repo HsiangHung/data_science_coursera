d0f50598e0ffec3e3588028cfac50b0bf1ea537e
Hsianghung2015 <hhhung@physics.utexas.edu>
2015-08-22 18:59:04 -0500

    assignment3-2

--- R_programming/assign3-rankhospital.R
+++ R_programming/assign3-rankhospital.R
@@ -1,8 +1,8 @@
 rankhospital <- function(state,outcome,num){
       ## Read outcome data
       data <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
-      statename <- data$State 
       
+      statename <- data$State 
       if (state %in% statename == FALSE) {
             stop('invalid state')
       }
@@ -25,27 +25,30 @@ rankhospital <- function(state,outcome,num){
       #min_numb = min(k,na.rm=TRUE)
       #min_posit = which.min(k)
       
-      sample_numb <- length(name)
+      sample_numb <- length(hospital_name)
+      #print(sample_numb)
       
       rank <- order(k,na.last =TRUE)
       raterank <- k[rank[1:sample_numb]]
-      b <- is.na(raterank)
+      b <- is.na(k)
       last_rank <- length(b[b==FALSE])
       namerank <- hospital_name[rank[1:sample_numb]]
+      
+      #print(data.frame(Hospital.Name=namerank,Rate=raterank,Rank=1:sample_numb))
       
       if (num =='best'){
             num =1
       } else if (num == 'worst') {
             num =last_rank
       }
-      result <- data.frame(Hospital.Name=namerank[1:num],Rate=raterank[1:num],Rank=1:num)
+      #print(num)
+      #print(data.frame(Hospital.Name=namerank[1:num],Rate=raterank[1:num],Rank=1:num))
       
       ## Check that state and outcome are valid
       #print(state)
-      #print(raterank)
       ## Return hospital name in that state with lowest 30-day death
       ## 30-day death rate
-      print(namerank[num])
+      result <- namerank[num]
       return(result)
 }
 
